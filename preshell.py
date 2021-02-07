d3d339d391659d48cd6ebc6f2c4d1ea2a3279a75
Joseph <jomedinas7>
2021-02-07 14:01:30 -0700

    whatever

--- preshell.py
+++ preshell.py
@@ -1,17 +1,22 @@
 import os
 
+def add_to_run(procList, args):
+    procList.append(os.fork())
+    if procList[-1] == 0:
+        os.execv('/usr/bin/'+args[0],args)
+
 def wait_for_all(processes):
     for procID in processes:
         os.waitpid(procID,0)
 
-def add_and_run(procList, args):
-        procList = procList + [os.fork()]
-        if procList[-1] == 0:
-            os.execv('usr/bin/'+args[0],args,os.environ)
-
 def main():
     proc = []
-    add_and_run(proc,['cat', 'proc/cpuinfo'])
-    add_and_run(proc,['echo', 'Hello World'])
-    add_and_run(proc,['python3', 'spinner.py 1000000'])
-    add_and_run(proc,['uname', '-a'])
+    add_to_run(proc,['cat', '/proc/cpuinfo'])
+    add_to_run(proc,['echo', 'Hello World'])
+    add_to_run(proc,['python3', 'spinner.py', '1000000'])
+    add_to_run(proc,['uname', '-a'])
+
+    wait_for_all(proc)
+    add_to_run(proc, ['python3', 'spinner.py', '2000000'])
+
+main()
