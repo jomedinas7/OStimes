f02535f99ad7c8a5694ac0e4cf9fba431cd936a6
Joseph <jomedinas7>
2021-02-07 21:17:53 -0700

    progress

--- shellAssignment/shell.py
+++ shellAssignment/shell.py
@@ -1,13 +1,18 @@
-import os
-import sys
+#Joseph Medina - Sandoval
 
+import os
+
+#from aa-chatbot.py
 class ConsoleReader:
     def __init__(self):
         pass
 
     def get_next_line(self):
-        return input('>> ').split(" ")
+        if os.getenv("PS1") == None:
+            return input('$$$$ ').split(" ")
+        return input(os.getenv("PS1")).split(" ")
 
+#from aa-chabot.py
 class FileReader:
     def __init__(self, filename):
         with open(filename) as f:
@@ -16,11 +21,11 @@ class FileReader:
 
     def get_next_line(self):
         line = self.file_lines.pop(0)
+        print('line:', line)
         if line[0] == '#':
-            print('input:', line)
-            return ''
+            line = self.file_lines.pop(0)
 
-        return line
+        return line.split(" ")
 
 class Shell:
     def __init__(self,theReader):
@@ -30,15 +35,17 @@ class Shell:
 
     def start_run(self):
         self.running = True
+        print('PS1: ',os.getenv("PS1"))
         self.run()
 
     def run(self):
         while self.running:
             self.argsList = self.reader.get_next_line()
+
             if len(self.argsList) == 1 and self.argsList[0] == 'exit':
-                print('Now exiting...')
                 self.exit_run()
             self.process_input()
+        return
 
     def exit_run(self):
         print('Exit successful.')
@@ -52,19 +59,37 @@ class Shell:
             print('cd found')
             os.chdir(self.argsList[1])
             return
-        if self.argsList[0] == 'ls':
-            os.execv('/usr/bin/ls', self.argsList)
-        if self.argsList[0] == 'cat':
-            os.execv('/usr/bin/cat', self.argsList)
-        if self.argsList[0] == 'grep':
-            os.execv('/usr/bin/grep', self.argsList)
-        return
+        if self.argsList[0][0:3] == 'PS1':
+            lolita = self.argsList[0].split("=")
+            os.environ['PS1']  = lolita[1]
+            return
+        #most functions to be called this way are in /usr/bin/
+        #streamline this input
+        #try-catch block
+        #class ProcessRunner?
+        try:
+            os.execv('/usr/bin/' + self.argsList[0], self.argsList)
+        except FileNotFoundError:
+            print("Invalid command")
+
+        # if self.argsList[0] == 'ls':
+        #     os.execv('/usr/bin/ls', self.argsList)
+        # if self.argsList[0] == 'cat':
+        #     os.execv('/usr/bin/cat', self.argsList)
+        # if self.argsList[0] == 'grep':
+        #     os.execv('/usr/bin/grep', self.argsList)
+        # if self.argsList[0][0:3] == 'PS1':
+        #     lolita = self.argsList[0].split("=")
+        #     os.environ['PS1']  = lolita[1]
 
 
 
 def main():
-
-    myReader = ConsoleReader()
+    choice = input('1. File \n2. Input\n>>')
+    if choice == '1':
+        myReader = FileReader('test.txt')
+    elif choice == '2':
+        myReader = ConsoleReader()
     myShell = Shell(myReader)
     myShell.start_run()
 
